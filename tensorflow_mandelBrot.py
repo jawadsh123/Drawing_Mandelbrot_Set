cb7a9e0510d564dd68cbc89ec07d389a318c61b1
Jawad <shaikhjawad007@gmail.com>
2017-04-22 23:56:11 +0530

    added roi

--- tensorflow_mandelBrot.py
+++ tensorflow_mandelBrot.py
@@ -3,30 +3,76 @@ import tensorflow as tf
 import cv2
 
 
-def DisplayFractal(a):
-	"""Display an array of iteration counts as a
-	 colorful picture of a fractal."""
-	a_cyclic = (6.28*a/20.0).reshape(list(a.shape)+[1])
-	img = np.concatenate([10+20*np.cos(a_cyclic),
-	                    30+50*np.sin(a_cyclic),
-	                    155-80*np.cos(a_cyclic)], 2)
-	img[a==a.max()] = 0
-	a = img
-	a = np.uint8(np.clip(a, 0, 255))
-	cv2.imshow("set", a/255.0)
-	cv2.waitKey(0)
-
-
-
-
+X, Y = 0,0
+Z = 0
 
 # Starting the sesssion right from the start
 sess = tf.InteractiveSession()
 
 
-# defining the grid for operations
-Y, X = np.mgrid[-1.3:1.3:0.005, -2.5:1:0.005]
-Z = X+1j*Y
+def Claculate_Colors(a):
+	"""Display an array of iteration counts as a
+	colorful picture of a fractal."""
+	a_cyclic = (6.28*a/20.0).reshape(list(a.shape)+[1])
+	img = np.concatenate([10+20*np.cos(a_cyclic),
+		30+50*np.sin(a_cyclic),
+		155-80*np.cos(a_cyclic)], 2)
+	img[a==a.max()] = 0
+	a = img
+	a = np.uint8(np.clip(a, 0, 255))
+
+	return a/255.0
+
+
+
+def Plot_Set(image):
+	
+	global start_x, start_y, end_x, end_y, draw_started, clone
+	clone = image.copy()
+	start_x, start_y = 0, 0
+	end_x, end_y = 0, 0
+	draw_started = False
+
+	def On_Mouse(event, x, y, flags, params):
+		global start_x, start_y, end_x, end_y, draw_started, clone
+
+		if event == cv2.EVENT_LBUTTONDOWN:
+			print('Start Mouse Position: '+str(x)+', '+str(y))
+			start_x = x
+			start_y = y
+			draw_started = True
+		if draw_started == True:
+			clone = image.copy()
+			cv2.rectangle(clone, (start_x, start_y), (x, y), (255,255,255), 2)
+			cv2.imshow("set", clone)
+		if event == cv2.EVENT_LBUTTONUP:
+	 		print('End Mouse Position: '+str(x)+', '+str(y))
+	 		end_x = x
+	 		end_y = y
+	 		cv2.rectangle(clone, (start_x, start_y), (end_x, end_y), (255,255,255), 2)
+	 		cv2.imshow("set", clone)
+	 		draw_started = False
+
+
+	cv2.namedWindow("set")
+	cv2.setMouseCallback("set", On_Mouse)
+
+	while True:
+		cv2.imshow("set", clone)
+
+
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			cv2.destroyAllWindows()
+			break
+
+
+Create_Iteration_Matrix(min_x=-2.5, max_x=1, min_y=1.3, max_y=1):
+
+	x_diff = max_x - min_x
+	step = x_diff/700
+
+	Y, X = np.mgrid[min_y:max_y:step, min_x:max_x:step]
+	Z = X+1j*Y
 
 # creating the matrices required
 cs = tf.constant(Z.astype(np.complex64))
@@ -46,10 +92,15 @@ update_zs = zs.assign(zs_new)
 update_count = ns.assign_add(tf.cast(not_diverged, tf.float32))
 
 
-for _ in range(200):
-	update_zs.eval()
-	update_count.eval()
 
-DisplayFractal(ns.eval())
+def main():
+	for _ in range(200):
+		update_zs.eval()
+		update_count.eval()
 
+	a = Claculate_Colors(ns.eval())
 
+	Plot_Set(a)
+
+main()
+cv2.destroyAllWindows()
