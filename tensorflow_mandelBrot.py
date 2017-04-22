f49fb285df46a78d11c152869dd84408ed78ae14
Jawad <shaikhjawad007@gmail.com>
2017-04-23 01:22:16 +0530

    zoom functionality

--- tensorflow_mandelBrot.py
+++ tensorflow_mandelBrot.py
@@ -14,9 +14,9 @@ def Claculate_Colors(a):
 	"""Display an array of iteration counts as a
 	colorful picture of a fractal."""
 	a_cyclic = (6.28*a/20.0).reshape(list(a.shape)+[1])
-	img = np.concatenate([10+20*np.cos(a_cyclic),
-		30+50*np.sin(a_cyclic),
-		155-80*np.cos(a_cyclic)], 2)
+	img = np.concatenate([155-80*np.cos(a_cyclic),
+						30+50*np.sin(a_cyclic),
+						10+20*np.cos(a_cyclic)], 2)
 	img[a==a.max()] = 0
 	a = img
 	a = np.uint8(np.clip(a, 0, 255))
@@ -29,8 +29,9 @@ def Plot_Set(image):
 	
 	global start_x, start_y, end_x, end_y, draw_started, clone
 	clone = image.copy()
+	height, width, channels = image.shape
 	start_x, start_y = 0, 0
-	end_x, end_y = 0, 0
+	end_x, end_y = width, height
 	draw_started = False
 
 	def On_Mouse(event, x, y, flags, params):
@@ -60,47 +61,77 @@ def Plot_Set(image):
 	while True:
 		cv2.imshow("set", clone)
 
+		
+		k = cv2.waitKey(1)
 
-		if cv2.waitKey(1) & 0xFF == ord('q'):
+		if k & 0xFF == ord(' '):
+			return (start_x, end_x, start_y, end_y, width, height)
+
+		if k & 0xFF == ord('q'):
 			cv2.destroyAllWindows()
-			break
+			return False
 
 
-Create_Iteration_Matrix(min_x=-2.5, max_x=1, min_y=1.3, max_y=1):
+def Create_Iteration_Matrix(min_x=-2.5, max_x=1, min_y=-1.3, max_y=1.3):
 
 	x_diff = max_x - min_x
 	step = x_diff/700
+	# print(step, max_x, min_x)
 
-	Y, X = np.mgrid[min_y:max_y:step, min_x:max_x:step]
+	Y, X = np.mgrid[min_y: max_y: step,  min_x: max_x: step]
 	Z = X+1j*Y
 
-# creating the matrices required
-cs = tf.constant(Z.astype(np.complex64))
-zs = tf.Variable(cs)
-ns = tf.Variable(tf.zeros_like(cs, tf.float32))
+	# Y, X = np.mgrid[0: 1,  0: 1]
+	# Z = X+1j*Y
 
-# initializing tensorflow variables
-tf.global_variables_initializer().run()
+	# creating the matrices required
+	cs = tf.constant(Z.astype(np.complex64))
+	zs = tf.Variable(cs)
+	ns = tf.Variable(tf.zeros_like(cs, tf.float32))
 
-# mandelbrot function
-zs_new = zs*zs + cs
+	# initializing tensorflow variables
+	tf.global_variables_initializer().run()
 
-# boolean matrix for checking if it has diverged
-not_diverged = tf.abs(zs_new) < 4
+	# mandelbrot function
+	zs_new = zs*zs + cs
 
-update_zs = zs.assign(zs_new)
-update_count = ns.assign_add(tf.cast(not_diverged, tf.float32))
+	# boolean matrix for checking if it has diverged
+	not_diverged = tf.abs(zs_new) < 4
 
+	update_zs = zs.assign(zs_new)
+	update_count = ns.assign_add(tf.cast(not_diverged, tf.float32))
 
-
-def main():
 	for _ in range(200):
 		update_zs.eval()
 		update_count.eval()
 
-	a = Claculate_Colors(ns.eval())
+	return ns.eval()
 
-	Plot_Set(a)
+def calculate_Set_Coordinates(temp, dim, max_val, min_val):
+	return  min_val + (temp/dim) * (max_val - min_val)
+
+
+def main():
+
+	min_x, max_x, min_y, max_y = -2.5, 1, -1.3, 1.3
+
+	while True:
+		print("Creating Plot")
+		ns = Create_Iteration_Matrix(min_x, max_x, min_y, max_y)
+		a = Claculate_Colors(ns)
+		returned_packet = Plot_Set(a)
+
+
+		if not returned_packet:
+			break
+		else:
+			temp_min_x, temp_max_x, temp_min_y, temp_max_y, width, height = returned_packet
+			new_min_x = calculate_Set_Coordinates(temp_min_x, width, max_x, min_x)
+			new_max_x = calculate_Set_Coordinates(temp_max_x, width, max_x, min_x)
+			new_min_y = calculate_Set_Coordinates(temp_min_y, height, max_y, min_y)
+			new_max_y = calculate_Set_Coordinates(temp_max_y, height, max_y, min_y)
+			min_x, max_x, min_y, max_y = new_min_x, new_max_x, new_min_y, new_max_y
+
 
 main()
 cv2.destroyAllWindows()
